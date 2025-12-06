e7988a2dacf514c6fad2df082e24588af79f68f3
hathienloc131 <ha.thienloc131@gmail.com>
2025-12-06 14:23:49 +0700

    [DataConfig] add vrh3 sim

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -1512,6 +1512,119 @@ class AlohaRightArmEffortConfig(BaseDataConfig):
 
         return ComposedModalityTransform(transforms=transforms)
     
+
+class VRH3EffortConfig(BaseDataConfig):
+    video_keys = ["video.cam_front"]
+    state_keys = [
+        "state.left_arm",
+        "state.right_arm",
+        "state.left_hand",
+        "state.right_hand",
+    ]
+    action_keys = [
+        "action.left_arm",
+        "action.right_arm",
+        "action.left_hand",
+        "action.right_hand",
+    ]
+    effort_keys = [
+        "effort.left_arm",
+        "effort.right_arm",
+        "effort.left_hand",
+        "effort.right_hand",
+    ]
+    language_keys = ["annotation.human.task_description"]
+    observation_indices = [0]
+    state_indices = [0]
+    action_indices = list(range(16))
+    effort_indices = list(range(1, 17))
+    
+    effort_dims=26
+
+    def modality_config(self):
+        video_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.video_keys,
+        )
+        state_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.state_keys,
+        )
+        effort_modality = ModalityConfig(
+            delta_indices=self.effort_indices,
+            modality_keys=self.effort_keys,
+        )
+        action_modality = ModalityConfig(
+            delta_indices=self.action_indices,
+            modality_keys=self.action_keys,
+        )
+        language_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.language_keys,
+        )
+        modality_configs = {
+            "video": video_modality,
+            "state": state_modality,
+            "effort": effort_modality,
+            "action": action_modality,
+            "language": language_modality,
+        }
+        return modality_configs
+
+    def transform(self):
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes={key: "min_max" for key in self.state_keys},
+                # target_rotations={
+                #     "state.end_effector_rotation_relative": "rotation_6d",
+                #     "state.base_rotation": "rotation_6d",
+                # },
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes={key: "min_max" for key in self.action_keys},
+            ),
+            StateActionToTensor(apply_to=self.effort_keys),
+            StateActionTransform(
+                apply_to=self.effort_keys,
+                normalization_modes={key: "min_max" for key in self.effort_keys},
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                effort_concat_order=self.effort_keys,
+                action_concat_order=self.action_keys,
+            ),
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                effort_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=32,
+                max_effort_dim=26,
+            ),
+        ]
+
+        return ComposedModalityTransform(transforms=transforms)
+    
 ###########################################################################################
     
 
@@ -1535,5 +1648,6 @@ DATA_CONFIG_MAP = {
     "vrh2_two_hand_2_cam": VRH2TwotHand2CamConfig(),
     "vrh2_two_hand_2_cam_vel_eff": VRH2TwotHand2CamVelEffConfig(),
     "aloha_right_arm_only": AlohaRightArmConfig(),
-    "aloha_right_arm_only_effort": AlohaRightArmEffortConfig()
+    "aloha_right_arm_only_effort": AlohaRightArmEffortConfig(),
+    "vrh3_effort": VRH3EffortConfig()
 }
