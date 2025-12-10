[Equi] temp fix masking

gr00t/model/action_head/flow_matching_action_head.py
@@ -722,18 +722,7 @@ class FlowmatchingActionHead(nn.Module):
             
             # Copy original mask values
             action_mask[:, :, :original_action_dim] = action_input.action_mask
-            
-            # Calculate the added dimensions from quaternion to 6D rotation transformation
-            # Each hand: quaternion (4D) -> 6D rotation (6D), so +2D per hand
-            # Two hands: +4D total
-            num_added_rotation_dims = 2 * 2  # 2 hands × 2 extra dimensions per hand
-            added_dims_start = original_action_dim
-            added_dims_end = added_dims_start + num_added_rotation_dims
-            
-            # Set mask to 1.0 for the added rotation dimensions
-            action_mask[:, :, added_dims_start:added_dims_end] = 1.0
-
-            
+            print(action_mask[0,0])
         loss = F.mse_loss(pred_actions, velocity, reduction="none") * action_mask
         loss = loss.sum() / action_mask.sum()
         output_dict = {

gr00t/model/transforms.py
@@ -294,7 +294,8 @@ class GR00TTransform(InvertibleModalityTransform):
 
         # Create mask: [T, max_action_dim]
         actions_mask = np.zeros((n_action_tokens, self.max_action_dim), dtype=bool)
-        actions_mask[:, :n_action_dims] = True
+        #TODO: hard code for 2 end effector
+        actions_mask[:, :n_action_dims + 2 * 2] = True 
 
         return actions, actions_mask, n_action_tokens
 