bcc596233a9898699cfb6f748c69ca8c65d786ac
hathienloc131 <ha.thienloc131@gmail.com>
2025-12-31 11:18:45 +0700

    [Libero] add data config goal mean std

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -1624,6 +1624,113 @@ class EquiLiberoConfig(BaseDataConfig):
 
         return ComposedModalityTransform(transforms=transforms)
     
+ 
+class EquiLiberoMeanStdConfig(BaseDataConfig):
+    video_keys = ["video.image", "video.wrist_image"]
+    state_keys = [
+        "state.x",
+        "state.y",
+        "state.z",
+        "state.rx",
+        "state.ry",
+        "state.rz",
+        "state.rw",
+        "state.gripper",
+    ]
+    action_keys = [
+        "action.x",
+        "action.y",
+        "action.z",
+        "action.rx",
+        "action.ry",
+        "action.rz",
+        "action.rw",
+        "action.gripper",
+    ]
+    language_keys = ["annotation.human.action.task_description"]
+    observation_indices = [0]
+    state_indices = [0]
+    action_indices = list(range(16))
+    num_hand = 1
+    rot_type="quaternion"
+
+    def modality_config(self):
+        video_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.video_keys,
+        )
+        state_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.state_keys,
+        )
+        action_modality = ModalityConfig(
+            delta_indices=self.action_indices,
+            modality_keys=self.action_keys,
+        )
+        language_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.language_keys,
+        )
+        modality_configs = {
+            "video": video_modality,
+            "state": state_modality,
+            "action": action_modality,
+            "language": language_modality,
+        }
+        return modality_configs
+
+    def transform(self):
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes={
+                    "state.x": "min_max",
+                    "state.y": "min_max",
+                    "state.z": "min_max",
+                    "state.gripper": "min_max",
+                },
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes={
+                    "action.x": "mean_std",
+                    "action.y": "mean_std",
+                    "action.z": "mean_std",
+                    "action.gripper": "mean_std",
+                },
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                action_concat_order=self.action_keys,
+            ),
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=32,
+                num_hand=self.num_hand
+            ),
+        ]
+
+        return ComposedModalityTransform(transforms=transforms)
     
 
 class EquiFractalDataConfig(BaseDataConfig):
@@ -1813,6 +1920,7 @@ DATA_CONFIG_MAP = {
     "aloha_right_arm_only": AlohaRightArmConfig(),
     "vrh3_two_hand_1_cam_equi": VRH3TwotHand1CamEquiConfig(),
     "equi_libero": EquiLiberoConfig(),
+    "equi_libero_mean_std": EquiLiberoMeanStdConfig(),
     "equi_bridge": BridgeDataConfig(),
     "equi_fractal": EquiFractalDataConfig(),
 }
