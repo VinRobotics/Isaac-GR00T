75b71519c410a52473f98ab455d03d31ef3d9d38
hathienloc131 <ha.thienloc131@gmail.com>
2026-02-03 12:03:16 +0700

    [FA] revert open eagle linear weight

--- gr00t/model/backbone/eagle_backbone_late_fa.py
+++ gr00t/model/backbone/eagle_backbone_late_fa.py
@@ -121,10 +121,10 @@ class EagleBackboneLateFa(nn.Module):
         self.eagle_model = AutoModel.from_config(config, trust_remote_code=True)
 
         # Projection layer
-        # if project_to_dim is not None:
-        self.eagle_linear = torch.nn.Linear(2048, project_to_dim)
-        # else:
-        #     self.eagle_linear = torch.nn.Identity()
+        if project_to_dim is not None:
+            self.eagle_linear = torch.nn.Linear(2048, project_to_dim)
+        else:
+            self.eagle_linear = torch.nn.Identity()
 
         # Remove unused LLM layers
         while len(self.eagle_model.language_model.model.layers) > select_layer:
