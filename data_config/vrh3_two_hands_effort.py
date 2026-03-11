9500db523fb1414663a927fb6102aa6ebfa15c56
hathienloc131 <ha.thienloc131@gmail.com>
2026-03-11 17:05:08 +0700

    vrh3_two_hands_effort

---
+++ data_config/vrh3_two_hands_effort.py
@@ -0,0 +1,69 @@
+from gr00t.configs.data.embodiment_configs import register_modality_config
+from gr00t.data.types import ModalityConfig, ActionConfig, ActionRepresentation, ActionType, ActionFormat
+
+vrh3_two_hands_config = {
+    "video": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["cam_head", "cam_left", "cam_right"],
+    ),
+    "state": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=[
+            "left_arm",
+            "right_arm",
+            "left_hand",
+            "right_hand",
+            "effort_left_arm",
+            "effort_right_arm",
+            "effort_left_hand",
+            "effort_right_hand",
+        ],
+        sin_cos_embedding_keys=[
+            "left_arm",
+            "right_arm",
+            "left_hand",
+            "right_hand",
+            "effort_left_arm",
+            "effort_right_arm",
+            "effort_left_hand",
+            "effort_right_hand",
+        ],
+    ),
+    "action": ModalityConfig(
+        delta_indices=list(range(0, 50)),
+        modality_keys=[
+            "left_arm",
+            "right_arm",
+            "left_hand",
+            "right_hand",
+        ],
+        action_configs=[
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+        ],
+    ),
+    "language": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["annotation.human.task_description"],
+    ),
+}
+
+register_modality_config(vrh3_two_hands_config)
