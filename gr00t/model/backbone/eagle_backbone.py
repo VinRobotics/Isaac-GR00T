c762da5d04995e30fb49a829cb3680c11baeb3a4
hathienloc131 <ha.thienloc131@gmail.com>
2026-03-11 08:33:29 +0700

    fix import

--- gr00t/model/backbone/eagle_backbone.py
+++ gr00t/model/backbone/eagle_backbone.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from ast import List
+from typing import Optional, List
 import os
 
 import torch
