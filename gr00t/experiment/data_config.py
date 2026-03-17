acf34af8afd0ffb0080a81f6c25ce7b8097622b4
hathienloc131 <ha.thienloc131@gmail.com>
2026-03-17 11:54:24 +0700

    update config

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -1719,12 +1719,9 @@ class VRH3_1EffortConfig(BaseDataConfig):
     def transform(self):
         transforms = [
             # video transforms
-            VideoToTensor(apply_to=self.video_keys[:1]),
-            VideoCrop(apply_to=self.video_keys[:1], scale=0.95),
-            VideoResize(apply_to=self.video_keys[:1], height=224, width=224, interpolation="linear"),
-            VideoToTensor(apply_to=self.video_keys[1:]),
-            VideoCrop(apply_to=self.video_keys[1:], scale=0.95),
-            VideoResize(apply_to=self.video_keys[1:], height=224, width=224, interpolation="linear"),
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
             VideoColorJitter(
                 apply_to=self.video_keys,
                 brightness=0.3,
