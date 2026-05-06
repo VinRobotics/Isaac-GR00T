97286183573fb22f52e60cc4bb22d12bc80296df
hathienloc131 <ha.thienloc131@gmail.com>
2026-05-06 15:11:31 +0700

    [Mimicgen] modify script

--- evaluation/run_check_equi_gr00t_dit.sh
+++ evaluation/run_check_equi_gr00t_dit.sh
@@ -4,9 +4,19 @@
 set -e
 
 python evaluation/check_env_preprocessing.py \
-    --branch-name equi_gr00t_dit \
-    --gr00t-path /home/locht1/gr00t_equi_fa \
-    --libero-path /mnt/data/sftp/data/locht1/LIBERO_benchmark \
-    --mimicgen-path /mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen \
-    --robosuite-assets /mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen/robosuite/models/assets \
-    --seed 7
+    --args.branch-name=equi_gr00t_dit \
+    --args.gr00t-path=/home/locht1/gr00t_equi_fa \
+    --args.libero-path=/mnt/data/sftp/data/locht1/LIBERO_benchmark \
+    --args.mimicgen-path=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen \
+    --args.robosuite-assets=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen/robosuite/models/assets \
+    --args.seed=7 --args.skip_libero
+
+python evaluation/check_env_preprocessing.py \
+    --args.branch-name=equi_gr00t_dit \
+    --args.gr00t-path=/home/locht1/gr00t_equi_fa \
+    --args.libero-path=/mnt/data/sftp/data/locht1/LIBERO_benchmark \
+    --args.mimicgen-path=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen \
+    --args.robosuite-assets=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen/robosuite/models/assets \
+    --args.seed=7 --args.skip_mimicgen
+
+
