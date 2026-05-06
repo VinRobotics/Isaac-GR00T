564e9e07c4c6418c4128e356e73930d261a9e73c
hathienloc131 <ha.thienloc131@gmail.com>
2026-05-06 15:15:02 +0700

    [Mimicgen] fix conda

--- evaluation/run_check_equi_gr00t_fa.sh
+++ evaluation/run_check_equi_gr00t_fa.sh
@@ -2,19 +2,18 @@
 # Run preprocessing capture for equi_gr00t_fa branch
 # Execute this from the repo root: bash evaluation/run_check_equi_gr00t_fa.sh
 set -e
-
+source /home/locht1/miniconda3/bin/activate gr00t_equi_fa_mimicgen
 python evaluation/check_env_preprocessing.py \
     --args.branch-name=equi_gr00t_fa \
     --args.gr00t-path=/home/locht1/gr00t_equi_fa \
     --args.libero-path=/mnt/data/sftp/data/locht1/LIBERO_benchmark \
     --args.mimicgen-path=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen \
-    --args.robosuite-assets=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen/robosuite/models/assets \
     --args.seed=7 --args.skip_libero
 
+source /home/locht1/miniconda3/bin/activate gr00t_equi_fa
 python evaluation/check_env_preprocessing.py \
     --args.branch-name=equi_gr00t_fa \
     --args.gr00t-path=/home/locht1/gr00t_equi_fa \
     --args.libero-path=/mnt/data/sftp/data/locht1/LIBERO_benchmark \
     --args.mimicgen-path=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen \
-    --args.robosuite-assets=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen/robosuite/models/assets \
     --args.seed=7 --args.skip_mimicgen
--- evaluation/run_check_gr00t_baseline.sh
+++ evaluation/run_check_gr00t_baseline.sh
@@ -2,19 +2,18 @@
 # Run preprocessing capture for gr00t_baseline branch (gr00t_baseline_locht1)
 # Execute this from the repo root: bash evaluation/run_check_gr00t_baseline.sh
 set -e
-
+source /home/locht1/miniconda3/bin/activate gr00t_mimicgen
 python evaluation/check_env_preprocessing.py \
     --args.branch-name=gr00t_baseline \
     --args.gr00t-path=/home/locht1/gr00t_equi_dit \
     --args.libero-path=/mnt/data/sftp/data/locht1/LIBERO_benchmark \
     --args.mimicgen-path=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen \
-    --args.robosuite-assets=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen/robosuite/models/assets \
     --args.seed=7 --args.skip_libero
 
+source /home/locht1/miniconda3/bin/activate gr00t
 python evaluation/check_env_preprocessing.py \
     --args.branch-name=gr00t_baseline \
     --args.gr00t-path=/home/locht1/gr00t_equi_dit \
     --args.libero-path=/mnt/data/sftp/data/locht1/LIBERO_benchmark \
     --args.mimicgen-path=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen \
-    --args.robosuite-assets=/mnt/data/sftp/data/locht1/mimicgen_evaluation/mimicgen/robosuite/models/assets \
     --args.seed=7 --args.skip_mimicgen
