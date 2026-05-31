dbd440093c17d67e5462155009cd90e25c8ad3a4
hathienloc131 <ha.thienloc131@gmail.com>
2026-05-31 18:38:25 +0700

    update data config

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -2207,7 +2207,6 @@ class VRH31GripperBothForRL(BaseDataConfig):
         return ComposedModalityTransform(transforms=transforms)
     
 ###########################################################################################
-
 class VRH31Gripper2ArmForRL(BaseDataConfig):
     video_keys = ["video.cam_head", "video.cam_left", "video.cam_right"]
     state_keys = [
@@ -2234,122 +2233,6 @@ class VRH31Gripper2ArmForRL(BaseDataConfig):
     language_keys = ["annotation.human.task_description"]
     reward_keys = [
         "reward.current",
-        "reward.current_frame_idx",
-        "reward.episode_lengths"
-    ]
-    observation_indices = [0]
-    state_indices = [0]
-    action_indices = [x * 2 for x in range(16)]
-
-    def modality_config(self):
-        video_modality = ModalityConfig(
-            delta_indices=self.observation_indices,
-            modality_keys=self.video_keys,
-        )
-        state_modality = ModalityConfig(
-            delta_indices=self.observation_indices,
-            modality_keys=self.state_keys,
-        )
-        action_modality = ModalityConfig(
-            delta_indices=self.action_indices,
-            modality_keys=self.action_keys,
-        )
-        language_modality = ModalityConfig(
-            delta_indices=self.observation_indices,
-            modality_keys=self.language_keys,
-        )
-        reward_modality = ModalityConfig(
-            delta_indices=self.observation_indices,
-            modality_keys=self.reward_keys
-        )
-        modality_configs = {
-            "video": video_modality,
-            "state": state_modality,
-            "action": action_modality,
-            "language": language_modality,
-            "reward": reward_modality
-        }
-        return modality_configs
-
-    def transform(self):
-        transforms = [
-            # video transforms
-            VideoToTensor(apply_to=self.video_keys[:1]),
-            VideoCrop(apply_to=self.video_keys[:1], scale=0.95),
-            VideoResize(apply_to=self.video_keys[:1], height=224, width=224, interpolation="linear"),
-            VideoToTensor(apply_to=self.video_keys[1:]),
-            VideoCrop(apply_to=self.video_keys[1:], scale=0.95),
-            VideoResize(apply_to=self.video_keys[1:], height=224, width=224, interpolation="linear"),
-            VideoColorJitter(
-                apply_to=self.video_keys,
-                brightness=0.3,
-                contrast=0.4,
-                saturation=0.5,
-                hue=0.08,
-            ),
-            VideoToNumpy(apply_to=self.video_keys),
-            # state transforms
-            StateActionToTensor(apply_to=self.state_keys),
-            StateActionTransform(
-                apply_to=self.state_keys,
-                normalization_modes={key: "min_max" for key in self.state_keys},
-                # target_rotations={
-                #     "state.end_effector_rotation_relative": "rotation_6d",
-                #     "state.base_rotation": "rotation_6d",
-                # },
-            ),
-            # action transforms
-            StateActionToTensor(apply_to=self.action_keys),
-            StateActionTransform(
-                apply_to=self.action_keys,
-                normalization_modes={key: "min_max" for key in self.action_keys},
-            ),
-            # concat transforms
-            ConcatTransform(
-                video_concat_order=self.video_keys,
-                state_concat_order=self.state_keys,
-                action_concat_order=self.action_keys,
-            ),
-            GR00TTransform(
-                state_horizon=len(self.observation_indices),
-                action_horizon=len(self.action_indices),
-                max_state_dim=64,
-                max_action_dim=32,
-            ),
-        ]
-
-        return ComposedModalityTransform(transforms=transforms)
-    
-###########################################################################################
-
-class VRH31Gripper2ArmForRL(BaseDataConfig):
-    video_keys = ["video.cam_head", "video.cam_left", "video.cam_right"]
-    state_keys = [
-        "state.left_shoulder",
-        "state.left_elbow",
-        "state.left_wrist",
-        "state.left_gripper_open",
-        "state.right_shoulder",
-        "state.right_elbow",
-        "state.right_wrist",
-        "state.right_gripper_open",
-    ]
-    action_keys = [
-        "action.left_shoulder",
-        "action.left_elbow",
-        "action.left_wrist",
-        "action.left_gripper_open",
-        "action.right_shoulder",
-        "action.right_elbow",
-        "action.right_wrist",
-        "action.right_gripper_open",
-        "action.task_progress",
-    ]
-    language_keys = ["annotation.human.task_description"]
-    reward_keys = [
-        "reward.current",
-        "reward.current_frame_idx",
-        "reward.episode_lengths"
     ]
     observation_indices = [0]
     state_indices = [0]
